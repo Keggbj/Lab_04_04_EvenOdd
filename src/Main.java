f7ed674e92066cfaaf00a5003cfbd3edcbc51768
minik <keggbj@mail.uc.edu>
2022-09-18 15:12:46 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int number = 33;
+        int remainder = number % 16;
+
+        if(remainder==1) {
+            System.out.println("odd number");
+        }
+        else
+        {
+            System.out.println("even number");}
+    }
+}
